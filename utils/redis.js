06e98b14e3964100fe478c30bd284b05518ac738
Vaal34 <5619@holbertonstudents.com>
2024-04-02 12:31:51 +0200

    fix syntaxe and remove .on("connect")

--- utils/redis.js
+++ utils/redis.js
@@ -1,38 +1,33 @@
 const redis = require('redis');
-const util = require('util')
+const { promisify } = require('util');
 
-
-class RedisClient{
-    constructor(){
-        this.client = redis.createClient()
-
-        this.client.on('connect', () => {
-            console.log("Redis client connected to the server");
-        })
+class RedisClient {
+    constructor() {
+        this.client = redis.createClient();
 
         this.client.on('error', (err) => {
-            console.log("Can't connect to Redis:", err)
-        })
+            console.log("Can't connect to Redis:", err);
+        });
     }
 
-    isAlive(){
-        return this.client.connected;                               
+    isAlive() {
+        return this.client.connected;
     }
 
-    async get(key){
-        const getAsync = util.promisify(this.client.get).bind(this.client);
-        return getAsync(key)
+    async get(key) {
+        const getAsync = promisify(this.client.get).bind(this.client);
+        return getAsync(key);
     }
 
-    async set(key, value, duration){
-        await this.client.set(key, value)
-        await this.client.expire(key, duration)
+    async set(key, value, duration) {
+        await this.client.set(key, value);
+        await this.client.expire(key, duration);
     }
 
-    async del(key){
-        await this.client.del(key)
+    async del(key) {
+        await this.client.del(key);
     }
 }
 
-const redisClient = new RedisClient()
+const redisClient = new RedisClient();
 module.exports = redisClient;
