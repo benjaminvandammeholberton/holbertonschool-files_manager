2996a153ffdea2074b3f6f12dd25fa9937a5ff5c
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-04-02 12:27:40 +0200

    create app router and set endpoints for status and stats

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-import express from 'express'
+import express from 'express';
+import router from './routes';
 
-const app = express()
-const port = 5000
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use('/', router);
 
 app.listen(port, () => {
-    console.log("Server running on port", port)
-})
+  console.log('Server running on port', port);
+});
