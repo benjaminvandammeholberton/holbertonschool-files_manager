c93b3020d033cf8caabceae92ad6c25aaf5423d6
Vaal34 <5619@holbertonstudents.com>
2024-04-02 12:39:59 +0200

    fix indent problem

--- utils/redis.js
+++ utils/redis.js
@@ -2,31 +2,31 @@ import { createClient } from 'redis';
 import { promisify } from 'util';
 
 class RedisClient {
-    constructor() {
-        this.client = createClient();
+  constructor() {
+    this.client = createClient();
 
-        this.client.on('error', (err) => {
-            console.log("Can't connect to Redis:", err);
-        });
-    }
+    this.client.on('error', (err) => {
+      console.log("Can't connect to Redis:", err);
+    });
+  }
 
-    isAlive() {
-        return this.client.connected;
-    }
+  isAlive() {
+    return this.client.connected;
+  }
 
-    async get(key) {
-        const getAsync = promisify(this.client.get).bind(this.client);
-        return getAsync(key);
-    }
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    return getAsync(key);
+  }
 
-    async set(key, value, duration) {
-        await this.client.set(key, value);
-        await this.client.expire(key, duration);
-    }
+  async set(key, value, duration) {
+    await this.client.set(key, value);
+    await this.client.expire(key, duration);
+  }
 
-    async del(key) {
-        await this.client.del(key);
-    }
+  async del(key) {
+    await this.client.del(key);
+  }
 }
 
 const redisClient = new RedisClient();
