1e869c3f990c3e2044f1a07ea94ab504edb064ef
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-04-02 11:38:00 +0200

    add DBClient class connected to mongoDB

--- utils/db.js
+++ utils/db.js
@@ -1,22 +1,34 @@
-class DBClient{
-    constructor(){
+import MongoClient from 'mongodb/lib/mongo_client';
 
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}`;
+    MongoClient.connect(url, { useUnifiedTopology: true }, (error, client) => {
+      this.db = client.db(database);
+    });
+  }
+
+  isAlive() {
+    if (this.db) {
+      return true;
     }
+    return false;
+  }
 
-    isAlive(){
-        return true
-    }
+  async nbUsers() {
+    const users = this.db.collection('users');
+    return users.countDocuments();
+  }
 
-    async nbUsers(){
-        return true
-    }
-
-    async nbFiles(){
-        return true 
-    }
-
+  async nbFiles() {
+    const files = this.db.collection('files');
+    return files.countDocuments();
+  }
 }
 
-const dbClient = new DBClient()
+const dbClient = new DBClient();
 
-export default dbClient
+export default dbClient;
