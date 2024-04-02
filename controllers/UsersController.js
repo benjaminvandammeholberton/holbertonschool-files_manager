f41ba2f0e14ef78afe1804d12af7b961a22a9864
Vaal34 <5619@holbertonstudents.com>
2024-04-02 21:42:11 +0200

    add endpoint that create a user

---
+++ controllers/UsersController.js
@@ -0,0 +1,32 @@
+import dbClient from '../utils/db';
+
+const sha1 = require('js-sha1');
+
+const UsersController = {
+  postNew: async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).send({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      return res.status(400).send({ error: 'Missing password' });
+    }
+
+    const usersCollection = dbClient.db.collection('users');
+    const user = await usersCollection.findOne({ email });
+    if (user) {
+      return res.status(400).send({ error: 'Already exist' });
+    }
+
+    const addNewUser = await usersCollection.insertOne({
+      email,
+      password: sha1(password),
+    });
+
+    return res.status(201).send({ email, id: addNewUser.insertedId });
+  },
+};
+
+export default UsersController;
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+import bodyParser from 'body-parser';
+
 import express from 'express';
 import router from './routes';
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 const port = process.env.PORT || 5000;
 
 app.use('/', router);
