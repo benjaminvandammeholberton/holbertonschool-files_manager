d002a7ac2b0608985ff1a8f8f8c393292eb5a941
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-04-03 20:35:34 +0200

    add: FilesController

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,9 +1,12 @@
 import { ObjectId } from 'mongodb';
+import fs from 'fs';
+import { v4 as uuidv4 } from 'uuid';
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 const FilesController = {
   postUpload: async (req, res) => {
+    // Get the user based on the token
     const token = req.headers['x-token'];
     if (!token) {
       return res.status(401).json({ error: 'Unauthorized' });
@@ -19,6 +22,7 @@ const FilesController = {
       return res.status(401).json({ error: 'Unauthorized' });
     }
 
+    // Verification of the request data
     const {
       name, type, parentId, isPublic, data,
     } = req.body;
@@ -43,17 +47,60 @@ const FilesController = {
         return res.status(400).json({ error: 'Parent is not a folder' });
       }
     }
+
+    // If folder, add to database
     if (type === 'folder') {
-      const document = {
+      const filesCollection = dbClient.db.collection('files');
+      const newFolder = {
         name,
         type,
         parentId: parentId || 0,
         isPublic: isPublic || false,
         userId: user._id,
       };
-      console.log(document)
+      await filesCollection.insertOne(newFolder);
+      newFolder.id = newFolder._id;
+      delete newFolder._id;
+      return res.status(201).json(newFolder);
     }
+
+    // File creation
+    const uuid = uuidv4();
+    const folderPath = process.env.FOLDER_PATH || '/tmp/files_manager';
+    const filePath = `${folderPath}/${uuid}`;
+
+    if (!fs.existsSync(folderPath)) {
+      fs.mkdirSync(folderPath, { recursive: true }, (err) => {
+        if (err) {
+          console.error('A problem occured when creating the directory', err);
+          res.status(500).end();
+        }
+      });
+    }
+    const decryptedData = Buffer.from(data, 'base64').toString('utf-8');
+    fs.writeFile(filePath, decryptedData, (err) => {
+      if (err) {
+        console.error('A problem occured when creating the file', err);
+        res.status(500).end();
+      }
+    });
+
+    // File to database
+    const newFile = {
+      name,
+      userId,
+      type,
+      isPublic: isPublic || false,
+      parentId: parentId || 0,
+      localPath: filePath,
+    };
+    const filesCollection = dbClient.db.collection('files');
+    await filesCollection.insertOne(newFile);
+    newFile.id = newFile._id;
+    delete newFile._id;
+    return res.status(201).json(newFile);
   },
+
 };
 
 export default FilesController;
