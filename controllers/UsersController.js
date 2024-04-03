6888d5d62d248dd8eb29da4dfbec66fd1602200a
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-04-03 10:22:22 +0200

    change import for sha1 and replace response.send by json

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,31 +1,30 @@
+import sha1 from 'sha1';
 import dbClient from '../utils/db';
-
-const crypto = require('crypto');
 
 const UsersController = {
   postNew: async (req, res) => {
     const { email, password } = req.body;
 
     if (!email) {
-      return res.status(400).send({ error: 'Missing email' });
+      return res.status(400).json({ error: 'Missing email' });
     }
 
     if (!password) {
-      return res.status(400).send({ error: 'Missing password' });
+      return res.status(400).json({ error: 'Missing password' });
     }
 
     const usersCollection = dbClient.db.collection('users');
     const user = await usersCollection.findOne({ email });
     if (user) {
-      return res.status(400).send({ error: 'Already exist' });
+      return res.status(400).json({ error: 'Already exist' });
     }
 
     const addNewUser = await usersCollection.insertOne({
       email,
-      password: crypto.createHash('sha1').update(password).digest('hex'),
+      password: sha1(password),
     });
 
-    return res.status(201).send({ email, id: addNewUser.insertedId });
+    return res.status(201).json({ email, id: addNewUser.insertedId });
   },
 };
 
