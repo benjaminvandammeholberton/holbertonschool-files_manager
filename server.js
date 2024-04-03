9efc1badb938f9d9158578aff090e8706b391dc6
Vaal34 <5619@holbertonstudents.com>
2024-04-03 10:41:15 +0200

    update parser json by express.json

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-import bodyParser from 'body-parser';
-
 import express from 'express';
 import router from './routes';
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 const port = process.env.PORT || 5000;
+
+app.use(express.json());
 
 app.use('/', router);
 
