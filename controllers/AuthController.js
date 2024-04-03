4e43c3f67c6bc86e2fe03db6a4e787b252a859f0
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-04-03 15:47:13 +0200

    fix: value to find the right key to delete in redis

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,5 +1,6 @@
 import sha1 from 'sha1';
 import { v4 as uuidv4 } from 'uuid';
+import { ObjectId } from 'mongodb';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
@@ -40,11 +41,18 @@ const AuthController = {
     if (!token) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
-    const userId = await redisClient.del(token);
+    const userId = await redisClient.get(`auth_${token}`);
     if (!userId) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
-    return res.status(204);
+    const _id = new ObjectId(userId);
+    const usersCollection = dbClient.db.collection('users');
+    const user = await usersCollection.findOne({ _id });
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    await redisClient.del(`auth_${token}`);
+    return res.status(204).end();
   },
 };
 
